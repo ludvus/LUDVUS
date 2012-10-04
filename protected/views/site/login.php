058144a10d567bec8fd003d0281dcf3e0b6a99c6
Vita <vita.reizupa@gmail.com>
2012-10-04 12:50:17 +0300

    pirmā versija+dizains

---
+++ protected/views/site/login.php
@@ -0,0 +1,38 @@
+<?php
+/* @var $this SiteController */
+/* @var $model LoginForm */
+/* @var $form CActiveForm  */
+
+$this->pageTitle=Yii::app()->name . ' - Pieslēgties';
+$this->breadcrumbs=array(
+	'Pieslēgties',
+);
+?>
+
+<div class="form">
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'login-form',
+	'enableClientValidation'=>true,
+	'clientOptions'=>array(
+		'validateOnSubmit'=>true,
+	),
+)); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'Lietotājvārds'); ?>
+		<?php echo $form->textField($model,'username'); ?>
+		<?php echo $form->error($model,'username'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'Parole'); ?>
+		<?php echo $form->passwordField($model,'password'); ?>
+		<?php echo $form->error($model,'password'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Pieslēgties'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+</div><!-- form -->
